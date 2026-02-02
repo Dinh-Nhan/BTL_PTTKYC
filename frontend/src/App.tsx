01b3db391e8664233ac0a341cbdedfcd5aa99b9b
Trung Le 05 <Trungrua951@gmail.com>
2026-02-02 23:15:21 +0700

    add Login UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,30 @@
 import { Route, Routes } from "react-router-dom";
-import Admin from "./pages/Admin";
-import RoomManagerPage from "./pages/Manager/RoomManagerPage";
-import DashBoardPage from "./pages/Manager/DashboardPage";
-import BookingPage from "./pages/Manager/BookingPage";
-import InvoicesPage from "./pages/Manager/InvoicesPage";
-import StaffPage from "./pages/Manager/StaffPage";
-import CustomerPage from "./pages/Manager/CustomerPage";
-
+// import Admin from "./pages/Admin";
+// import RoomManagerPage from "./pages/Manager/RoomManagerPage";
+// import DashBoardPage from "./pages/Manager/DashboardPage";
+// import BookingPage from "./pages/Manager/BookingPage";
+// import InvoicesPage from "./pages/Manager/InvoicesPage";
+// import StaffPage from "./pages/Manager/StaffPage";
+// import CustomerPage from "./pages/Manager/CustomerPage";
+import Login from "./pages/Login";
 const App = () => {
+
   return (
     // <Home />
-    <Admin>
-      <Routes>
-        <Route path="/" element={<DashBoardPage />} />
-        <Route path="rooms" element={<RoomManagerPage />} />
-        <Route path="bookings" element={<BookingPage />} />
-        <Route path="invoices" element={<InvoicesPage />} />
-        <Route path="staff" element={<StaffPage />} />
-        <Route path="customers" element={<CustomerPage />} />
-      </Routes>
-    </Admin>
+
+    // <Admin>
+    //   <Routes>
+    //     <Route path="/" element={<DashBoardPage/>} />
+    //     <Route path="rooms" element={<RoomManagerPage />} />
+    //     <Route path="bookings" element={<BookingPage />} />
+    //     <Route path="invoices" element={<InvoicesPage />} />
+    //     <Route path="staff" element={<StaffPage />} />
+    //     <Route path="customers" element={<CustomerPage />} />
+    //   </Routes>
+    // </Admin>
+    <Routes>
+      <Route path="/login" element={<Login/>} />
+    </Routes>
   );
 };
 
