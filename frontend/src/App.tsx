11a80d0688351cc7ba9b47139cf0084ace10d088
Dinh-Nhan <nhan01282544104@gmail.com>
2026-02-03 21:29:31 +0700

    init folder api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,32 @@
 import { Route, Routes } from "react-router-dom";
-import Admin from "./pages/Admin";
-import RoomManagerPage from "./pages/Manager/RoomManagerPage";
-import DashBoardPage from "./pages/Manager/DashboardPage";
-import BookingPage from "./pages/Manager/BookingPage";
-import InvoicesPage from "./pages/Manager/InvoicesPage";
-import StaffPage from "./pages/Manager/StaffPage";
-import CustomerPage from "./pages/Manager/CustomerPage";
+// import Admin from "./pages/Admin";
+// import RoomManagerPage from "./pages/Manager/RoomManagerPage";
+// import DashBoardPage from "./pages/Manager/DashboardPage";
+// import BookingPage from "./pages/Manager/BookingPage";
+// import InvoicesPage from "./pages/Manager/InvoicesPage";
+// import StaffPage from "./pages/Manager/StaffPage";
+// import CustomerPage from "./pages/Manager/CustomerPage";
+import Home from "./pages/Home";
+import RoomDetail from "./pages/RoomDetail";
 
 const App = () => {
   return (
-    // <Home />
-    <Admin>
-      <Routes>
-        <Route path="/" element={<DashBoardPage />} />
-        <Route path="rooms" element={<RoomManagerPage />} />
-        <Route path="bookings" element={<BookingPage />} />
-        <Route path="invoices" element={<InvoicesPage />} />
-        <Route path="staff" element={<StaffPage />} />
-        <Route path="customers" element={<CustomerPage />} />
-      </Routes>
-    </Admin>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/room/:id" element={<RoomDetail />} />
+    </Routes>
+
+    // <Admin>
+    //   <Routes>
+
+    //     <Route path="/admin" element={<DashBoardPage />} />
+    //     <Route path="/admin/rooms" element={<RoomManagerPage />} />
+    //     <Route path="/admin/bookings" element={<BookingPage />} />
+    //     <Route path="/admin/invoices" element={<InvoicesPage />} />
+    //     <Route path="/admin/staff" element={<StaffPage />} />
+    //     <Route path="/admin/customers" element={<CustomerPage />} />
+    //   </Routes>
+    // </Admin>
   );
 };
 
